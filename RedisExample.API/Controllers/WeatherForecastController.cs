263a645fb1a30cd582e72415179923d4ab619a47
pawelkupka <pawel.kupka@gmail.com>
2020-11-15 16:49:58 +0100

    api created

---
+++ RedisExample.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RedisExample.API.Controllers
+{
+    using Services;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly WeatherForecastService _weatherForecastService;
+
+        public WeatherForecastController(WeatherForecastService weatherForecastService)
+        {
+            _weatherForecastService = weatherForecastService;
+        }
+
+        [HttpGet]
+        public Task<IEnumerable<WeatherForecast>> Get()
+        {
+            return _weatherForecastService.GetWeatherForecastAsync();
+        }
+    }
+}
---
+++ RedisExample.API/Services/WeatherForecastService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisExample.API.Services
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecastAsync()
+        {
+            await Task.Delay(5000);
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
